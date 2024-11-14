a85a168cb93b1117c78406b21299378acc70a2db
Mateus Oliveira <123783867+MateusOliveiraT25@users.noreply.github.com>
2024-11-14 09:00:43 -0300

    14/11/24

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'screens/login_screen.dart';
-import 'screens/home_screen.dart';
 import 'screens/todo_list_screen.dart';
 
 void main() async {
@@ -22,14 +20,12 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false, // Desativa o banner de debug
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        // Aqui você pode configurar o tema para o app conforme desejado
       ),
       initialRoute: '/',
       routes: {
         '/': (context) => LoginScreen(),
-        '/home': (context) => HomeScreen(),
         '/todo': (context) => TodoListScreen(),
-        '/login': (context) => LoginScreen(), // Rota de login
+        '/login': (context) => LoginScreen(),
       },
     );
   }
